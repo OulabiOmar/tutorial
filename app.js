c6ad081a7e57d081178f18b4ad96c7d7e49ef1d2
Mr. Omaro <thekingomar2010@gmail.com>
2024-02-22 19:25:17 +0300

    new 3rd commit

--- app.js
+++ app.js
@@ -8,3 +8,5 @@ if (amount < 10){
 
 console.log("hi first app here!!")
 console.log("adding new code to commit to github test");
+
+console.log("this is the 3rd commit test lets see what will happen !!");
