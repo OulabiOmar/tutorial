6d27e877b2cafa8627a632f4742f9e93ffd0f738
Mr. Omaro <thekingomar2010@gmail.com>
2024-02-22 19:26:50 +0300

    nah

--- app.js
+++ app.js
@@ -10,3 +10,4 @@ console.log("hi first app here!!")
 console.log("adding new code to commit to github test");
 
 console.log("this is the 3rd commit test lets see what will happen !!");
+console.log("what if i didnt push the 3rd and commit again the 4th time and then we push");
